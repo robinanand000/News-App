980ad513d625623dc1cfabf8b348ce589fdeb983
Robins Anand <107298444+robinanand000@users.noreply.github.com>
2022-08-17 00:24:39 +0530

    adding final files

--- src/components/News.js
+++ src/components/News.js
@@ -1,22 +1,79 @@
 import React, { Component } from 'react'
 import NewsItem from './NewsItem'
+import PropTypes from 'prop-types'
+import Spinner from './Spinner';
 
 export class News extends Component {
+
+  static defaultProps={
+    country:'in',
+    pageSize:9,
+    category:"general",
+  }
+  static propTypes={
+    country: PropTypes.string,
+    pageSize: PropTypes.number,
+    category: PropTypes.string,
+  }
+
+  capitalizeFirstLetter=(str)=>{
+    return str.charAt(0).toUpperCase() + str.slice(1);
+  }
+
+  constructor(props){
+    super(props);
+    this.state={
+      articles:[],
+      loading: false,
+      page:1
+    }
+    document.title= `${this.capitalizeFirstLetter(this.props.category)} -NewsMonkey`;
+  }
+  
+async updateNews(){
+  let url=`https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=063a20a3197648538a1302df80bca019&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+    this.setState({loading:true})
+    let data= await fetch(url);
+    let parsedData=await data.json();
+    console.log(parsedData);
+    this.setState({
+      articles:parsedData.articles,
+      totalResults:parsedData.totalResults,
+      loading:false,
+    })
+}
+ async  componentDidMount(){
+  this.updateNews();
+  }
+
+  handleNextClick= async()=>{
+    this.setState({page:this.state.page+1})
+    this.updateNews();
+  }
+
+  handlePrevClick= async()=>{
+    this.setState({page:this.state.page-1})
+    this.updateNews();
+  }
+
   render() {
     return (
       <div className='container my-3'>
-        <h2>NewsMonkey - Top Headlines</h2>
-        <div className="row">
-            <div className="col-md-4">
-                <NewsItem title="mytitle" description="mydesc" />
-            </div>
-            <div className="col-md-4">
-                <NewsItem title="mytitle" description="mydesc" />
-            </div>
-            <div className="col-md-4">
-                <NewsItem title="mytitle" description="mydesc" />
-            </div>
-        </div>
+          <h2>Top Headlines - {this.capitalizeFirstLetter(this.props.category)}</h2>
+          {this.state.loading &&<Spinner/>}
+
+          <div className="row ">
+            {!this.state.loading && this.state.articles.map((element)=>{
+              return <div className="col-md-4" key={element.url}>
+              <NewsItem date={element.publishedAt} title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} source={element.source.name}/>
+              </div>
+            })}
+          </div>
+
+          {!this.state.loading &&<div className="container d-flex justify-content-evenly my-4">
+              <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Prev</button>
+              <button disabled={this.state.page+1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+          </div>}
       </div>
     )
   }
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -3,16 +3,18 @@ import React, { Component } from 'react'
 export class NewsItem extends Component {
   render() {
 
-    let {title,description} = this.props;
+    let {date,title,description,imageUrl,newsUrl,author,source} = this.props;
 
     return (
       <div>
-        <div className="card" style={{width: "18rem"}}>
-            <img src="https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg" className="card-img-top" alt="..."/>
+        <div className="card my-4">
+            <img src={!imageUrl?"https://images.hindustantimes.com/tech/img/2022/08/14/1600x900/iPhone-14-Pro-Purple-Front-and-Back-MacRumors-Exclusive_1653967629359_1660489027078_1660489027078.jpeg":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
+            <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:'1'}}>{source}</span>
                 <h5 className="card-title">{title}</h5>
                 <p className="card-text">{description}</p>
-                <a href="/newsItem" className="btn btn-primary">Go somewhere</a>
+                <p className="card-text"><small className="text-info">By {author?author:"Unknown"} on {new Date(date).toGMTString()} </small></p>
+                <a href={newsUrl} rel="noreferrer" target="_blank" className="btn btn-sm btn-dark">Read More</a>
             </div>
         </div>
       </div>
